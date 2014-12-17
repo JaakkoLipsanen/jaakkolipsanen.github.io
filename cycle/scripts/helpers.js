7a342627bac168d634e7abff0c5a86b81120264c
JaakkoLipsanen <jaakko.lipsanen@outlook.com>
2014-12-17 03:44:36 +0200

    Continuation..

--- cycle/scripts/helpers.js
+++ cycle/scripts/helpers.js
@@ -24,5 +24,5 @@ function exitFullScreen() {
 }
 
 function isFullScreen() {
-	return ((document.fullscreenElement && document.fullscreenElement !== null) || document.mozFullScreen || document.webkitIsFullScreen;
+	return (document.fullscreenElement && document.fullscreenElement !== null) || document.mozFullScreen || document.webkitIsFullScreen;
 }
