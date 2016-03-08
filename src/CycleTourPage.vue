4793a9ba0e5a50b3cf6d9a274612c04a19472165
JaakkoLipsanen <jaakko.lipsanen@outlook.com>
2016-03-08 16:38:50 +0200

    Some tweaking + added new blog post to europe15
    Former-commit-id: 2aa1da461ae1501ceea06b1cef187c7690fe1a03

--- src/CycleTourPage.vue
+++ src/CycleTourPage.vue
@@ -1,14 +1,19 @@
 <template>
-	<div class="blog-list-page-container">
-		<div class="blog-post-block" v-for="post in blog.PostInfos.reverse()">
-			<div class="blog-post-block-background" style="background-image: url({{ post.Directory + '1080p/' + post.MainImage  }});" v-on:click="postClicked(post)">
-				<div class="post-block-container">
-					<h1 style="top: 50%; position: relative; transform: translateY(-50%); color: rgb(190, 190, 190); font-family: 'Yanone Kaffeesatz'; font-size: 3em; margin: 0">{{ post.Title }}</h1>
-					<h3 style="top: 47%; position: relative; transform: translateY(-50%); color: rgb(190, 190, 190); font-family: 'Yanone Kaffeesatz'; font-size: 1.75em; margin: 0"> {{ 'Day ' + post.DateRange }} </h3>
+	<div>
+		<cycle-map style="padding-top: 128px; width: 70%; height: 500px"></cycle-map>
+		<p style="font-size: 24px; color: white; margin: auto; margin-top: 64px; text-align: center;">blogi</p>
+
+		<div class="blog-list-page-container">
+			<div class="blog-post-block" v-for="post in blog.PostInfos.reverse()">
+				<div class="blog-post-block-background" style="background-image: url({{ post.Directory + '1080p/' + post.MainImage  }});" v-on:click="postClicked(post)">
+					<div class="post-block-container">
+						<h1 style="top: 50%; position: relative; transform: translateY(-50%); color: rgb(190, 190, 190); font-family: 'Yanone Kaffeesatz'; font-size: 3em; margin: 0">{{ post.Title }}</h1>
+						<h3 style="top: 47%; position: relative; transform: translateY(-50%); color: rgb(190, 190, 190); font-family: 'Yanone Kaffeesatz'; font-size: 1.75em; margin: 0"> {{ 'Day ' + post.DateRange }} </h3>
+					</div>
 				</div>
 			</div>
-		</div>
 
+		</div>
 	</div>
 </template>
 
@@ -25,6 +30,7 @@ export default {
 	},
 
 	components: {
+		"cycle-map": CycleMap,
 	},
 
 	ready: function() {
@@ -70,7 +76,7 @@ export default {
 
 .blog-list-page-container {
 	margin: auto;
-	margin-top: 256px;
+	margin-top: 8px;
 	width: 90%;
 	display: flex;
 
