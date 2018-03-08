c5219aaf25c011fedd56abc343aaae927cbe774b
Jaakko Lipsanen <jaakko.lipsanen@reaktor.fi>
2018-03-09 00:37:43 +0200

    Add day range & trip name to CoverImage

--- src/components/CoverImage.tsx
+++ src/components/CoverImage.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 import withProps from 'styled-components-ts';
 
 import aws from '../aws';
-import { ImageQuality } from '../common';
+import { ImageQuality, formatDateRange } from '../common';
 import { BlogPost } from '../blog';
 import { TakeHeight } from './helpers';
 
@@ -37,15 +37,43 @@ const ImageOverlay = styled.div`
 `;
 
 const ImageTitle = styled.h2`
-	color: white;
-	height: 100%;
-	text-align: center;
-	transform: translate(0, 50%);
-	margin: 0;
-
 	position: absolute;
 	top: 0;
+
 	width: 100%;
+	height: 100%;
+	transform: translate(0, 50%);
+	text-align: center;
+	margin: 0;
+
+	color: #f9f9f9;
+	font-size: 34px;
+	font-weight: 300;
+	text-shadow: 1px 1px #000;
+`;
+
+const ImageSubtextContainer = styled.div`
+	position: absolute;
+	top: 0;
+
+	width: 100%;
+	height: 85vh;
+	padding-bottom: 22px;
+	box-sizing: border-box;
+
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: flex-end;
+
+	color: #f1f1f1;
+`;
+
+const ImageSubtextItem = styled.p`
+	font-size: 24px;
+	font-weight: 300;
+	font-style: italic;
+	margin: 2px;
 `;
 
 interface CoverImageProps { blogPost: BlogPost; height: string; }
@@ -57,7 +85,11 @@ const CoverImage = ({ blogPost, height }: CoverImageProps) => {
 			<ImageContainer height={height}>
 				<Image src={aws.getImageUrl(blogPost.name, ImageQuality.FullHD, blogPost.coverImage)} />
 				<ImageOverlay />
-				<ImageTitle>{title}</ImageTitle>
+				<ImageTitle>{title.toUpperCase()}</ImageTitle>
+				<ImageSubtextContainer>
+					<ImageSubtextItem>{formatDateRange(blogPost.dateRange)}</ImageSubtextItem>
+					<ImageSubtextItem>{blogPost.trip}</ImageSubtextItem>
+				</ImageSubtextContainer>
 			</ImageContainer>
 			<TakeHeight height={height} />
 		</>
